61828a7a66fb7c04477588ccd5a3d53fdcec0849
Carolina Martes <carolina.martes@gmail.com>
2016-11-02 15:48:28 -0400

    Add tutorial work v6

--- app/index.js
+++ app/index.js
@@ -1,12 +1,25 @@
+
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
+var Raven = require('raven-js')
 
-var Hello = React.createClass({
-  render: function () {
-    return (
-      <div>Hello ReactJS Program!</div>
-    )
+// Get your keys by signing up for Sentry at sentry.reactjsprogram.com
+var sentryKey = '7f99010ffdbd431481137274aa07d2de'
+var sentryApp = '109631'
+var sentryURL = 'https://' + sentryKey + '@app.getsentry.com/' + sentryApp
+
+var _APP_INFO = {
+  name: 'Github Battle',
+  branch: 'video4',
+  version: '1.0'
+}
+
+Raven.config(sentryURL, {
+  release: _APP_INFO.version,
+  tags: {
+    branch: _APP_INFO.branch,
   }
-});
+}).install()
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
